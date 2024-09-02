00a8e2eb9b036b8fdf13465121981a6dd1432ea5
ildus51 <galyavill@gmail.com>
2024-09-02 19:00:01 +0300

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,85 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print("Такого этажа не существует")
+        else:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        return False
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        return False
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        return False
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        return False
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        return True
+
+    def add(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+        return self
+
+    def __add__(self, value):
+        return self.add(value)
+
+    def __radd__(self, value):
+        return self.add(value)
+
+    def __iadd__(self, value):
+        return self.add(value)
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2)  # eq
+
+h1 = h1 + 10  # add
+print(h1)
+print(h1 == h2)
+
+h1 += 10  # iadd
+print(h1)
+
+h2 = 10 + h2  # radd
+print(h2)
+
+print(h1 > h2)  # gt
+print(h1 >= h2)  # ge
+print(h1 < h2)  # lt
+print(h1 <= h2)  # le
+print(h1 != h2)  # ne
